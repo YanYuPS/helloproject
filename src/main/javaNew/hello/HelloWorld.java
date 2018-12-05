cdca3c0ea0ff5480bfb05a01da8f672fb452227d
YanYuPS <18515952332@163.com>
2018-12-05 18:08:42 +0800

    修改测试输出

--- src/main/javaNew/hello/HelloWorld.java
+++ src/main/javaNew/hello/HelloWorld.java
@@ -2,8 +2,10 @@ package hello;
 
 public class HelloWorld {
 
-    public static void main(String args[]){
+    public static void main(String args[])
+    {
         System.out.print("Hello World");
+        System.out.print("修改测试");
     }
 
 
